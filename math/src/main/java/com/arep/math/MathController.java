a1975d084652d36c56e7ccdd47345e87defb68c8
SergioBejarano <sergio.bejarano-r@mail.escuelaing.edu.co>
2025-10-22 10:28:05 -0500

    feat: arrreglo lista por parámetro

--- math/src/main/java/com/arep/math/MathController.java
+++ math/src/main/java/com/arep/math/MathController.java
@@ -10,28 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
     
-    private List<Integer> list = new ArrayList<>();
 
 
     @GetMapping("/secuence")
     public String getSecuence(@RequestParam(value = "value") String n){
 
         int value = Integer.parseInt(n);
-
-        secuenceLucas(value,2,1);
-        String response = responseJson(n);
+        List<Integer> list = new ArrayList<>();
+        secuenceLucas(value,2,1, list);
+        String response = responseJson(n, list);
         return response;
 
     }
 
-    public void secuenceLucas(Integer n, int a, int b){
+    public void secuenceLucas(Integer n, int a, int b, List<Integer> list){
         
         if (n==0) return;
         list.add(a);
-        secuenceLucas(n-1, b, a+b);
+        secuenceLucas(n-1, b, a+b, list);
     }
 
-    public String responseJson(String value){
+    public String responseJson(String value, List<Integer> list){
         String response = "{\n" +
                 " \"operation\":" + "\"" + "Secuencia de Lucas" + "\"" + ",\n" +
                 " \"input\":" + "\"" + value + "\"" + ",\n" +

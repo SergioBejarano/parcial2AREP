4652a36e00853b86d0dbbf48d9b2a611fc25fb8f
SergioBejarano <sergio.bejarano-r@mail.escuelaing.edu.co>
2025-10-22 09:09:35 -0500

    feat: add client and http connection

--- proxy/src/main/java/com/arep/proxy/ProxyController.java
+++ proxy/src/main/java/com/arep/proxy/ProxyController.java
@@ -1,5 +1,6 @@
 package com.arep.proxy;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProxyController {
 
+    private int number = 1;
 
+    @GetMapping("secuence")
+    public String getSecuence(@RequestParam(value = "value") String n) throws IOException{
+        String base;
+        if (number == 1){
+            number = 2;
+            base = "http://localhost:8081";
+
+        } else{
+            number = 1;
+            base = "http://localhost:8082";
+        }
+        String fullbase = base + "?value=" + n;
+        return HttpConnection.get(fullbase);
+
+    }
 }
